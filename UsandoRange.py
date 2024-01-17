30b8a13de95cd866581963c549bd1e0f6c660d89
Nicolás Fluxá <fluxa.andres@gmail.com>
2024-01-17 12:25:09 -0300

    Trucos con Range()

---
+++ UsandoRange.py
@@ -0,0 +1,11 @@
+
+# Genera una lista con 10 numeros desde el cero
+print(list(range(10)))  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Genera una lista de 5 numeros partiendo desde el cinco
+print(list(range(5, 10)))  # [5, 6, 7, 8, 9]
+# Genera una lista desde el 0 de tres en tres
+print(list(range(0, 10, 3)))  # [0, 3, 6, 9]
+# Lo mismo que en la linea 3 solo que hacia los negativos
+print(list(range(0, -10, -1)))  # [0, -1, -2, -3, -4, -5, -6, -7, -8, -9]
+# Inicio 5 Final -5 de -2 en -2 hasta 3
+print(list(range(5, -5, -2)))  # [5, 3, 1, -1, -3]
