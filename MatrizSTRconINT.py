b266ed465169117a73b107bfd871d0fc4e036aa5
Nicolás Fluxá <fluxa.andres@gmail.com>
2024-02-17 20:17:03 -0300

    Forma de mostrar matriz con str y integer

---
+++ MatrizSTRconINT.py
@@ -0,0 +1,7 @@
+# Supongamos que tienes la siguiente matriz
+matriz = [["Juan", 85], ["Ana", 90], ["Carlos", 78]]
+
+# Para recorrer la matriz, puedes usar un bucle for anidado
+for fila in matriz:
+    for elemento in fila:
+        print(elemento)
